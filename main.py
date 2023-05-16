697050de4ea854b915f3c521ab281c0af9d5e6c7
= <=>
2023-05-16 11:59:11 +0400

    tests

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+from func.extract_csv_data import extract_csv_data
+from func.get_account_info import get_account_info
+
 def run() -> None:
-    print("Hello, world!")
+    raw_csv_data = extract_csv_data(path = "input.csv")
+
+    account = get_account_info(raw_csv_data)
+
+    for key, value in account.items():
+        print(f"{key}: {value}")
 
 if __name__ == "__main__":
     run()
