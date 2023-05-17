8edb5b5e502e7762601f7f373f82ffc55d9094d5
= <=>
2023-05-17 10:53:10 +0400

    renaming the file

---
+++ func/format_bnp_to_ynab_csv.py
@@ -0,0 +1,8 @@
+def format_bnp_to_ynab_csv(file_path: str = "input.csv") -> None:
+    """
+    This function take the csv file and format it to the correct way
+    returns nothing because we want to write it to the disk
+
+
+    """
+    
